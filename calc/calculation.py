4976135a62711a66a47c5b054b0488a1e98e225f
Sandra <xo.sandra@live.com>
2021-11-11 19:32:15 -0500

    Added new directory "calc" with 2 new python files: calculation.py and addition.py

---
+++ calc/calculation.py
@@ -0,0 +1,8 @@
+"""This is our Calculation Base Class/Abstract Class"""
+class Calculation:
+
+    #contstructor and it is the first function called an object of the class is instantiated (created)
+    def __init__(self, value_a, value_b):
+        self.value_a = value_a
+        self.value_b = value_b
+
