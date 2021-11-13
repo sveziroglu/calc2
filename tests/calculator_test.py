7d8cedc6154b30f625ab284ce80371dd091ac370
Sandra <xo.sandra@live.com>
2021-11-13 17:33:46 -0500

    Adjusting tests for all operations.

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -10,18 +10,18 @@ def clear_history():
     Calculator.clear_history()
 
 def test_calculator_add(clear_history):
-    assert Calculator.add_number(1,2) == 3
-    assert Calculator.add_number(2, 2) == 4
-    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.add_number(7,3) == 10
+    assert Calculator.add_number(6, 6) == 12
+    assert Calculator.add_number(7, 7) == 14
     assert Calculator.add_number(4, 2) == 6
     assert Calculator.history_count() == 4
     assert Calculator.get_result_of_last_calculation_added_to_history() == 6
     pprint.pprint(Calculator.history)
 
 def test_clear_history(clear_history):
-    assert Calculator.add_number(1,2) == 3
-    assert Calculator.add_number(2, 2) == 4
-    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.add_number(7,3) == 10
+    assert Calculator.add_number(6, 6) == 12
+    assert Calculator.add_number(7, 7) == 14
     assert Calculator.add_number(4, 2) == 6
     assert Calculator.history_count() == 4
     assert Calculator.clear_history() == True
@@ -29,8 +29,8 @@ def test_clear_history(clear_history):
 
 def test_count_history(clear_history):
     assert Calculator.history_count() == 0
-    assert Calculator.add_number(2, 2) == 4
-    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.add_number(8, 8) == 16
+    assert Calculator.add_number(9, 9) == 18
     assert Calculator.history_count() == 2
 
 def test_get_last_calculation_result(clear_history):
