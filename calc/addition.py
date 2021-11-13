85b968d6f6d69467841ac9f26549a14c361fce77
Sandra <xo.sandra@live.com>
2021-11-13 17:58:42 -0500

    fixed newline missing error on pylint

--- calc/addition.py
+++ calc/addition.py
@@ -3,7 +3,3 @@ from calc.calculation import Calculation
 class Addition(Calculation):
     def getResult(self):
         return self.value_a + self.value_b
-
-
-
-
--- calc/calculation.py
+++ calc/calculation.py
@@ -3,6 +3,7 @@ class Calculation:
     def __init__(self,value_a, value_b):
         self.value_a = value_a
         self.value_b = value_b
+
     @classmethod
     def create(cls, value_a, value_b):
         return cls(value_a,value_b)
