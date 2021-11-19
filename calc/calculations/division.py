364fb19d0af48592a515026f8a264b9167a8dd4b
Sandra <xo.sandra@live.com>
2021-11-18 23:18:53 -0500

    cleaned up

---
+++ calc/calculations/division.py
@@ -0,0 +1,11 @@
+"""This is the division class"""
+from calc.calculations.calculation import Calculation
+
+class Division(Calculation):
+    """This is division"""
+
+    def get_result(self):
+        """This gives the quotient"""
+        quotient = self.values[1]
+        quotient = quotient/values
+        return quotient
