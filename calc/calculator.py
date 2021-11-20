639b003ffc51df3df5737388f069cb779c47a74e
Sandra <xo.sandra@live.com>
2021-11-19 22:39:28 -0500

    added division

--- calc/calculator.py
+++ calc/calculator.py
@@ -1,17 +1,15 @@
 """ This is the increment function"""
 from calc.history.calculations import Calculations
 
-#the calculator class just contains the methods to calculate
+
 class Calculator:
     """ This is the Calculator class"""
-    #the calculator class just calls methods on Calculations class
+
     @staticmethod
     def get_result_value():
-        """ This is the gets the result of the calculation"""
-        # I made this method so that I don't have more than one action per function
+        """ This gets the result of the calculation"""
         return Calculations.get_last_calculation_result_value()
     @staticmethod
-    #tuple allows me to pass in as many values as a I want
     def add_numbers(tuple_values: tuple):
         """ adds list of numbers"""
         Calculations.add_addition_calculation(tuple_values)
