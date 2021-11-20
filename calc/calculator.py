14df97bf46583d4b8efb04f53f96b0f9c47b47ba
sveziroglu <90662018+sveziroglu@users.noreply.github.com>
2021-11-19 22:40:56 -0500

    Update calculator.py

--- calc/calculator.py
+++ calc/calculator.py
@@ -1,17 +1,14 @@
 """ This is the increment function"""
 from calc.history.calculations import Calculations
 
-#the calculator class just contains the methods to calculate
 class Calculator:
     """ This is the Calculator class"""
-    #the calculator class just calls methods on Calculations class
+
     @staticmethod
     def get_result_value():
         """ This is the gets the result of the calculation"""
-        # I made this method so that I don't have more than one action per function
         return Calculations.get_last_calculation_result_value()
     @staticmethod
-    #tuple allows me to pass in as many values as a I want
     def add_numbers(tuple_values: tuple):
         """ adds list of numbers"""
         Calculations.add_addition_calculation(tuple_values)
@@ -26,7 +23,6 @@ class Calculator:
         """ multiplication number from result"""
         Calculations.add_multiplication_calculation(tuple_values)
         return True
-
     @classmethod
     def divide_numbers(cls, tuple_values: tuple):
         """divide numbers"""
